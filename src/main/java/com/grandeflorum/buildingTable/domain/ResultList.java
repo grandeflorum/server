be390d6c6042289898a76efe6413705f1c91c3a7
梦引牵魂 <MYQH@github.com>
2019-11-08 20:34:45 +0800

    修改服务

--- src/main/java/com/grandeflorum/buildingTable/domain/ResultList.java
+++ src/main/java/com/grandeflorum/buildingTable/domain/ResultList.java
@@ -34,6 +34,17 @@ public class ResultList {
     @Column(name="ZMJ")
     private double zmj;
 
+    @Column(name="ID")
+    private String id;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 
     public String getXmmc() {
         return xmmc;
